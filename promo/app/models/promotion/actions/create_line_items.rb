0c93c1bd2912ae9a40d673290dc52266b03079cd
David North <davidnorth@railsdog.com>
2011-06-06 11:27:08 +0100

    Working on Admin UI for promo action to create line items

--- promo/app/models/promotion/actions/create_line_items.rb
+++ promo/app/models/promotion/actions/create_line_items.rb
@@ -1,5 +1,9 @@
 class Promotion::Actions::CreateLineItems < PromotionAction
 
+  has_many :promotion_action_line_items, :foreign_key => 'promotion_action_id'
+
+  attr_accessor :line_items_string
+
   def perform(options = {})
 
   end
--- promo/public/admin/javascripts/promotions.js
+++ promo/public/admin/javascripts/promotions.js
@@ -19,7 +19,8 @@ var initProductRuleSourceField = function(){
 
 };
 
-var initProductActionCalculatorFields = function(){
+var initProductActions = function(){
+
   $('.calculator-fields').each(function(){
     var $fields_container = $(this);
     var $type_select = $fields_container.find('.type-select');
@@ -40,11 +41,63 @@ var initProductActionCalculatorFields = function(){
       }
     });
   });
+
+
+  //
+  // CreateLineItems Promotion Action
+  //
+  // Autocomplete product and populate variant select
+  $(".promotion_action.create_line_items input[name='add_product_name']").autocomplete("/admin/products.json?authenticity_token=" + $('meta[name=csrf-token]').attr("content"), {
+      parse: prep_autocomplete_data,
+      formatItem: function(item) {
+        return format_autocomplete(item);
+      }
+    }).result(function(event, data, formatted) {
+      if(data){
+        // $('#add_product_id').val(data.product.id);
+        var url = "/admin/products/" + data.product.permalink + "/variants.json?authenticity_token=" + $('meta[name=csrf-token]').attr("content");
+        var $variant_select = $("select[name='add_line_item_variant_id']");
+        $variant_select.html('');
+        $.getJSON(url, {}, function(variants_data){
+          $.each(variants_data, function(){
+            $variant_select.append($("<option />").val(this.id).text(this.label));
+          });
+        });
+      }
+    }
+    );
+  // Add line item to list
+  $(".promotion_action.create_line_items button.add").click(function(){
+    var $container = $(this).parents('.promotion_action');
+    var product_name = $container.find("input[name='add_product_name']").val();
+    var variant_id = $container.find("select[name='add_line_item_variant_id']").val();
+    var variant_name = $container.find("select[name='add_line_item_variant_id'] option:selected").text();
+    var quantity = $container.find("input[name='add_quantity']").val();
+    if(variant_id){
+      // Add to the table
+      var newRow = "<tr><td>" + product_name + "</td><td>" + variant_name + "</td><td>" + quantity + "</td></tr>";
+      $container.find('table').append(newRow);
+      // Add to serialized string in hidden text field
+      var $hiddenField = $container.find("input[type='hidden']");
+      $hiddenField.val($hiddenField.val() + "," + variant_id + "x" + quantity);
+    }
+    return false;
+  });
+  // Remove line item
+
+
+
+
+
+
+
+
 }
 
 $(document).ready(function() {
   initProductRuleSourceField();
-  initProductActionCalculatorFields();
+  initProductActions();
 });
+
 
 
